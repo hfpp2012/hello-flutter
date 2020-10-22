39fa12cb86d63f8a0cfd1d42e4fd5bab276c3cc3
hfpp2012 <hfpp2012@gmail.com>
2020-10-22 16:19:41 +0800

    card widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,55 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() {
   runApp(
-    MaterialApp(home: Card()),
+    MaterialApp(home: QuoteList()),
   );
 }
 
-class Card extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _CardState createState() => _CardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _CardState extends State<Card> {
-  int cardLevel = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(text: 'be youself 0be youself 0be youself 0', author: 'hfpp2012'),
+    Quote(text: 'be youself 1be youself 0be youself 0', author: 'hfpp2012'),
+    Quote(text: 'be youself 1be youself 0be youself 0', author: 'hfpp2012'),
+  ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+        margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  quote.text,
+                  style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+                ),
+                SizedBox(height: 6.0),
+                Text(
+                  quote.author,
+                  style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
+                ),
+              ]),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.grey[200],
         appBar: AppBar(
-          title: Text('ID Card'),
-          centerTitle: true,
-          backgroundColor: Colors.grey[850],
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                cardLevel += 1;
-              });
-            },
-            child: Icon(Icons.add),
-            backgroundColor: Colors.grey[800]),
-        body: Padding(
-            padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Center(
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/thumb.jpg'),
-                    radius: 40.0,
-                  ),
-                ),
-                Divider(height: 90.0, color: Colors.grey[800]),
-                Text('NAME',
-                    style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
-                SizedBox(height: 10.0),
-                Text('Chun-Li',
-                    style: TextStyle(
-                        color: Colors.amberAccent[200],
-                        letterSpacing: 2.0,
-                        fontSize: 28.0,
-                        fontWeight: FontWeight.bold)),
-                SizedBox(height: 30.0),
-                Text('CURRENT LEVEL',
-                    style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
-                SizedBox(height: 10.0),
-                Text('$cardLevel',
-                    style: TextStyle(
-                        color: Colors.amberAccent[200],
-                        letterSpacing: 2.0,
-                        fontSize: 28.0,
-                        fontWeight: FontWeight.bold)),
-                SizedBox(height: 30.0),
-                Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.grey[400],
-                    ),
-                    SizedBox(width: 10.0),
-                    Text('hfpp2012@gmail.com',
-                        style: TextStyle(
-                            color: Colors.grey[400],
-                            fontSize: 18.0,
-                            letterSpacing: 1.0))
-                  ],
-                )
-              ],
-            )));
+            title: Text('Awesome Quotes'),
+            centerTitle: true,
+            backgroundColor: Colors.redAccent),
+        body: Column(
+          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        ));
   }
 }
