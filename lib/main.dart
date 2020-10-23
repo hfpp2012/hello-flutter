856c1bf9797045688ac66faa1ac7d51cf9f0fb85
hfpp2012 <hfpp2012@gmail.com>
2020-10-23 15:23:08 +0800

    quote card

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() {
   runApp(
@@ -19,27 +20,6 @@ class _QuoteListState extends State<QuoteList> {
     Quote(text: 'be youself 1be youself 0be youself 0', author: 'hfpp2012'),
   ];
 
-  Widget quoteTemplate(quote) {
-    return Card(
-        margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Text(
-                  quote.text,
-                  style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
-                ),
-                SizedBox(height: 6.0),
-                Text(
-                  quote.author,
-                  style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
-                ),
-              ]),
-        ));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,7 +29,7 @@ class _QuoteListState extends State<QuoteList> {
             centerTitle: true,
             backgroundColor: Colors.redAccent),
         body: Column(
-          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+          children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
         ));
   }
 }
